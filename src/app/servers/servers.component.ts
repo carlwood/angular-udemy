bdcb62cccf7f2379c94064e0ba8931e305221b0c
Carl <cjwood17@gmail.com>
2018-04-18 17:00:16 +0100

    Grab serverName from user input

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-servers',
@@ -7,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
+  allowNewServer = false
+  serverCreationStatus = 'No server was created'
+  serverName = ''
 
   constructor() {
     setTimeout(() => {
-      this.allowNewServer = true;
+      this.allowNewServer = true
     }, 2000);
   }
 
@@ -20,7 +21,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created'
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value
   }
 
 }
