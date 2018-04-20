f0de5e48cd7e3142d48de9cbeb23e3c98a27d333
Carl <cjwood17@gmail.com>
2018-04-20 16:48:53 +0100

    If/else with ng-template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core'
 export class ServersComponent implements OnInit {
 
   allowNewServer = false
-  serverCreationStatus = 'No server was created'
-  serverName = ''
+  serverCreationStatus: String = 'No server was created'
+  serverName: String = ''
+  serverCreated = false
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`
+    this.serverCreated = true
   }
 
   onUpdateServerName(event: Event) {
